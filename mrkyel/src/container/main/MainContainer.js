be40b3063de70ed773f40c02e160a225279b37c3
alpoxdev <alpoxdev@gmail.com>
2021-02-02 00:15:28 +0900

    MainContainer register feedback - useLocation 이용

--- mrkyel/src/container/main/MainContainer.js
+++ mrkyel/src/container/main/MainContainer.js
@@ -1,9 +1,19 @@
+import { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import ContentsBox from 'components/ContentsBox/ContentsBox';
 import Login from 'components/Login/Login';
 import React from 'react';
 
 const MainContainer = () => {
-  if (window.location.pathname === '/register') return null;
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  const isRegister = useMemo(() => pathname === '/register', [pathname]);
+  if (isRegister) return null;
+
+  //   if (window.location.pathname === '/register') return null;
+
   return (
     <>
       <Login />
