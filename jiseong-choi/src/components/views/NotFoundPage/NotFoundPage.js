a7b693dce6590dff7a00754311cdc142f0c427f4
jiseong-choi <jiseong1951@kakao.com>
2021-02-03 01:28:42 +0900

    editTHeNotFoundPage

--- jiseong-choi/src/components/views/NotFoundPage/NotFoundPage.js
+++ jiseong-choi/src/components/views/NotFoundPage/NotFoundPage.js
@@ -4,7 +4,7 @@ import './NotFoundPage.css'
 function NotFoundPage() {
     return (
         <div className="not-found__container">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/qZtY8Q0AXbQ?autoplay=1&mute=1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen autoPlay="1"></iframe>
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/qZtY8Q0AXbQ?autoplay=1&mute=1" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen autoPlay="1"></iframe>
         </div>
     )
 }
