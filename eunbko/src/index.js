04ff0a88ac5e4e6df6aa2dfdfe1517f47797f074
EunBKo <minniemice98@gmail.com>
2021-02-03 10:23:25 +0900

    add router / eslint-prettier

--- eunbko/src/index.js
+++ eunbko/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {RootRouter} from "./routers";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RootRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
