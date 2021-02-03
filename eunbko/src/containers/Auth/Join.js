2bf2fa8f6765582dea7c14cb0e3461245cb8695d
EunBKo <minniemice98@gmail.com>
2021-02-03 15:50:45 +0900

    Add : Router & Set Folder Structure

---
+++ eunbko/src/containers/Auth/Join.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function AuthJoinContainer() {
+  return <>회원가입 만들어라잉</>;
+}
---
+++ eunbko/src/containers/Main/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function MainContainer() {
+  return <>메인페이지입니다</>;
+}
